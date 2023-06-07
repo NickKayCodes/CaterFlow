f8aecee1be2ed4e3c5f0e5fa0ae0dfb4dd35e31b
Nicky Kayyarath <nkayyarath@gmail.com>
2023-06-07 19:35:24 -0400

    Initialization of backend Spring Boot Project.

---
+++ CaterFlowBackEnd/src/main/java/com/krath/CaterFlowBackEnd/CaterFlowBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.krath.CaterFlowBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaterFlowBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaterFlowBackEndApplication.class, args);
+	}
+
+}
---
+++ CaterFlowBackEnd/src/test/java/com/krath/CaterFlowBackEnd/CaterFlowBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krath.CaterFlowBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaterFlowBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
