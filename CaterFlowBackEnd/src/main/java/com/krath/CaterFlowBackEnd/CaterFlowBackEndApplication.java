14f862eb344cd164457a888e75b6dbc0e989b746
Nicky Kayyarath <nkayyarath@gmail.com>
2023-07-27 18:24:58 -0400

    added crossorigins to caterflowbackendapplication to establish connection to front end server

--- CaterFlowBackEnd/src/main/java/com/krath/CaterFlowBackEnd/CaterFlowBackEndApplication.java
+++ CaterFlowBackEnd/src/main/java/com/krath/CaterFlowBackEnd/CaterFlowBackEndApplication.java
@@ -2,7 +2,9 @@ package com.krath.CaterFlowBackEnd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200/")
 @SpringBootApplication
 public class CaterFlowBackEndApplication {
 
