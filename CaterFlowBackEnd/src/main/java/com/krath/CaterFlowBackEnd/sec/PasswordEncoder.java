adb3202da5773ae54cd9e3d470b00c10e574563d
Nicky Kayyarath <nkayyarath@gmail.com>
2023-07-27 18:12:43 -0400

    created a password encoder class to use bcrypt to encrypt user password

---
+++ CaterFlowBackEnd/src/main/java/com/krath/CaterFlowBackEnd/sec/PasswordEncoder.java
@@ -0,0 +1,21 @@
+package com.krath.CaterFlowBackEnd.sec;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PasswordEncoder {
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    public PasswordEncoder(){
+        this.passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    public String encodePassword(String plainTextPw){
+        return passwordEncoder.encode(plainTextPw);
+    }
+
+    public boolean passwordMatch(String plainTextPw, String encodedPw){
+        return passwordEncoder.matches(plainTextPw, encodedPw);
+    }
+}
