5dd03cc71420480335ece781de3b31a205489d7a
Nicky Kayyarath <nkayyarath@gmail.com>
2023-07-27 18:31:02 -0400

    cobbled together a navbar... will update as I build out the application

---
+++ CaterFlowGUI/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit} from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(private router: Router){
+    
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+  
+}
