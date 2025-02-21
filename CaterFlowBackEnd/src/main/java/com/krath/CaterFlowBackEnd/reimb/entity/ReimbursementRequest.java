1b12cb5c2e85cab09e0807b0df08f36b2e6ff14e
Nicky Kayyarath <nkayyarath@gmail.com>
2025-02-21 16:32:06 -0500

    started on reimbursement feature.

---
+++ CaterFlowBackEnd/src/main/java/com/krath/CaterFlowBackEnd/reimb/entity/ReimbursementRequest.java
@@ -0,0 +1,33 @@
+package com.krath.CaterFlowBackEnd.reimb.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import com.krath.CaterFlowBackEnd.user.enitity.User;
+import com.krath.CaterFlowBackEnd.reimb.entity.ReimbStatus;
+
+@Entity
+@Table(name = "reimbursement_requests")
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class ReimbursementRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private double amount;
+    private String reason;
+    private byte[] recieptFile;
+    private ReimbStatus status = ReimbStatus.PENDING;
+
+
+}
